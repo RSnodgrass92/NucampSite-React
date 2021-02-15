f5b89ef6d80f8a9e40b54a3235d5582c4e0e8aa5
Ryan Snodgrass <rsnodgra92@gmail.com>
2021-02-15 16:42:28 -0700

    Fetch Handling Errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -13,18 +13,43 @@ export const addComment = (campsiteId, rating, author, text) => ({
 });
 
 
-
+//double arrow function is using react thunk to enable
 export const fetchCampsites = () => dispatch => {
 
     dispatch(campsitesLoading());
 
     // a call to fetch returns a promise
     return fetch(baseUrl + "campsites")
+        // first check to see if we get any response back at all, if not catch
+        .then(response=> {
+            // response.ok will only be truthy if the http status code is in the successful range 200-299
+            if (response.ok) {
+                return response; 
+            }
+            else{
+                //status and status text are built in props of the response object
+                const error= new Error(`Error ${response.status}: ${response.statusText}`)
+                error.response = response; 
+                //sends directly to the next catch method
+                throw error;
+            } 
+        }, 
+            // no response from the server at all
+            error =>
+            {
+                const errMess= new Error(error.message);
+                throw errMess;
+            }
+        )
+
     // when the promise resolves this then method will use the json method to convert
     // the response from json to javascript, this will be the array of campsites
         .then(response => response.json())
     // the json method returns a new promise for which the converted javascript array is a new response value when it resolves. 
         .then(camps => dispatch(addCampsites(camps)))
+
+    //if any of these promises are rejected or if a throw keyword is reached they will be caught by the catch, 
+        .catch(error => dispatch(campsitesFailed(error.message)))
 };
 
 export const campsitesLoading = () => ({
@@ -43,12 +68,26 @@ export const addCampsites = campsites => ({
 
 
 
-export const fetchComments= () => dispatch => {
-
-    return fetch(baseUrl+"comments")
-    .then(response=> response.json())
-    .then(comments => dispatch(addComments(comments)))
-}
+export const fetchComments = () => dispatch => {
+    return fetch(baseUrl + 'comments')
+        .then(response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => {
+                const errMess = new Error(error.message);
+                throw errMess;
+            }
+        )
+        .then(response => response.json())
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)));
+};
 
 export const commentsFailed = errMess => ({
     type: ActionTypes.COMMENTS_FAILED,
@@ -66,8 +105,23 @@ export const fetchPromotions = () => dispatch => {
     dispatch(promotionsLoading());
 
     return fetch(baseUrl + 'promotions')
+        .then(response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => {
+                const errMess = new Error(error.message);
+                throw errMess;
+            }
+        )
         .then(response => response.json())
-        .then(promotions => dispatch(addPromotions(promotions)));
+        .then(promotions => dispatch(addPromotions(promotions)))
+        .catch(error => dispatch(promotionsFailed(error.message)));
 };
 
 export const promotionsLoading = () => ({
